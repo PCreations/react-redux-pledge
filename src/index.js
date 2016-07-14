1aab1c54a384111080353a97096a6e88479bb0e4
Pierre Criulanscy <pcriulan@gmail.com>
2016-07-14 12:08:39 +0200

    Removed old import

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import pledgesMiddleware from './pledgesMiddleware'
 import createPledge from './createPledge'
 import withPledges from './withPledges'
-import pledgeSaga from './pledgeSaga'
 
 export {
     pledgesMiddleware,
